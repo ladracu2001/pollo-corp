649f2a32cbbb45bd8ad70105dbf0858917fc8621
ladracu2001 <ladracu2001@gmail.com>
2025-06-28 23:59:24 -0300

    Se agrega una nueva funcionalidad para hacer reportes. Pendiente un repository de analisis de clientela.

---
+++ src/main/java/ar/com/laboratorio/steady/pollo_corp/registro/aplication/ClientReportUseCase.java
@@ -0,0 +1,40 @@
+package ar.com.laboratorio.steady.pollo_corp.registro.aplication;
+
+import ar.com.laboratorio.steady.pollo_corp.registro.dominio.ports.out.ClientPrinterRepository;
+import java.io.File;
+
+import org.w3c.dom.Text;
+import org.w3c.dom.html.HTMLDocument;
+/**
+ * Use case for generating and retrieving client reports.
+ * This class interacts with the ClientReportRepository to perform operations related to client reports.
+ */
+public class ClientReportUseCase {
+
+    private final ClientPrinterRepository clientPrinterRepository;
+
+    public ClientReportUseCase(ClientPrinterRepository clientReportRepository) {
+        this.clientPrinterRepository = clientReportRepository;
+    }
+
+    public String getClientTextReport(String clientId) {
+        return clientPrinterRepository.getClientTextReport(clientId);
+    }
+
+    public Text getClientTextReportAsText(String clientId) {
+        return clientPrinterRepository.getClientTextReportAsText(clientId);
+    }
+
+    public byte[] exportClientData(String clientId, String format) {
+        return clientPrinterRepository.exportClientData(clientId, format);
+    }
+
+    public HTMLDocument getClientReportWeb(String clientId, String format) {
+        return clientPrinterRepository.getClientReportWeb(clientId, format);
+    }
+
+    public File getClientReportFile(String clientId, String format) throws Exception {
+        return clientPrinterRepository.getClientReportFile(clientId, format);
+    }
+
+}
