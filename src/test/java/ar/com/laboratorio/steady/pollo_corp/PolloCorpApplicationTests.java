1825716c5b8405c4876c5300676417697d6725d9
ladracu2001 <ladracu2001@gmail.com>
2025-06-29 00:24:07 -0300

    Se comentan los test de bd porque no hay imple/infra realizada.

--- src/test/java/ar/com/laboratorio/steady/pollo_corp/PolloCorpApplicationTests.java
+++ src/test/java/ar/com/laboratorio/steady/pollo_corp/PolloCorpApplicationTests.java
@@ -1,15 +1,15 @@
 package ar.com.laboratorio.steady.pollo_corp;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+//TODO: descomentar cuando este listo el testing y su infra.
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.context.annotation.Import;
+//
+//@Import(TestcontainersConfiguration.class)
+//@SpringBootTest
 class PolloCorpApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+//	@Test
+//	void contextLoads() {
+//	}
+//
 }
--- src/test/java/ar/com/laboratorio/steady/pollo_corp/TestcontainersConfiguration.java
+++ src/test/java/ar/com/laboratorio/steady/pollo_corp/TestcontainersConfiguration.java
@@ -1,18 +1,18 @@
 package ar.com.laboratorio.steady.pollo_corp;
+//TODO: descomentar cuando este listo el testing y su infra.
+//import org.springframework.boot.test.context.TestConfiguration;
+//import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+//import org.springframework.context.annotation.Bean;
+//import org.testcontainers.containers.MySQLContainer;
+//import org.testcontainers.utility.DockerImageName;
 
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.utility.DockerImageName;
-
-@TestConfiguration(proxyBeanMethods = false)
+//@TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
 
-	@Bean
-	@ServiceConnection
-	MySQLContainer<?> mysqlContainer() {
-		return new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
-	}
-
+//	@Bean
+//	@ServiceConnection
+//	MySQLContainer<?> mysqlContainer() {
+//		return new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
+//	}
+//
 }
