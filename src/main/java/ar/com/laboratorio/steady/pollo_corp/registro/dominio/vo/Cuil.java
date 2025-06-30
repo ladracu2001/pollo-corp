9ba4c6196fbfdfe6c677fcc9bd4787dfbcc917a4
ladracu2001 <ladracu2001@gmail.com>
2025-06-29 22:03:12 -0300

    Remocion de digito verificador.

--- src/main/java/ar/com/laboratorio/steady/pollo_corp/registro/dominio/vo/Cuil.java
+++ src/main/java/ar/com/laboratorio/steady/pollo_corp/registro/dominio/vo/Cuil.java
@@ -12,30 +12,8 @@ public record Cuil(String valor) {
         if (!valor.matches("\\d{2}-\\d{8}-\\d")) {
             throw new IllegalCUILException("Formato inválido de CUIL: " + valor);
         }
-
-        if (!esValido(valor)) {
-            throw new IllegalCUILException("CUIL inválido según dígito verificador");
-        }
     }
 
-    private static boolean esValido(String cuil) {
-        // Eliminar guiones
-        String digits = cuil.replaceAll("-", "");
-        if (digits.length() != 11 || !digits.matches("\\d+")) return false;
-
-        int[] pesos = {5, 4, 3, 2, 7, 6, 5, 4, 3, 2};
-        int suma = 0;
-
-        for (int i = 0; i < 10; i++) {
-            suma += Character.getNumericValue(digits.charAt(i)) * pesos[i];
-        }
-
-        int resto = suma % 11;
-        int verificadorCalculado = resto == 0 ? 0 : resto == 1 ? 9 : 11 - resto;
-        int verificadorReal = Character.getNumericValue(digits.charAt(10));
-
-        return verificadorCalculado == verificadorReal;
-    }
     @Override
     public String toString() {
         return valor;
