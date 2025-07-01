834ff7809acd24c069956389d6124fc7f762c593
ladracu2001 <ladracu2001@gmail.com>
2025-07-01 15:34:42 -0300

    Arreglos menores.

--- src/main/java/ar/com/laboratorio/steady/pollo_corp/registro/infrastructure/api/dtos/ClientResponseDto.java
+++ src/main/java/ar/com/laboratorio/steady/pollo_corp/registro/infrastructure/api/dtos/ClientResponseDto.java
@@ -1,13 +1,22 @@
 package ar.com.laboratorio.steady.pollo_corp.registro.infrastructure.api.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClientResponseDto {
-    public String cuil;
-    public String dni;
-    public String name;
-    public String surname;
-    public String lastName;
-    public String birthDate;
-    public String email;
-    public String phoneNumber;
-    public String address;
+    private String cuil;
+    private String dni;
+    private String name;
+    private String surname;
+    private String lastName;
+    private String birthDate;
+    private String email;
+    private String phoneNumber;
+    private String address;
 }
