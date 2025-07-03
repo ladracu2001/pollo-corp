9fd846dfb499423f766c853c20a9df0e2e6f90dc
ladracu2001 <ladracu2001@gmail.com>
2025-07-03 14:50:05 -0300

    Validacion de inyeccion del bean en contenedor.

--- src/test/java/ar/com/laboratorio/steady/pollo_corp/PolloCorpApplicationTests.java
+++ src/test/java/ar/com/laboratorio/steady/pollo_corp/PolloCorpApplicationTests.java
@@ -1,15 +1,23 @@
 package ar.com.laboratorio.steady.pollo_corp;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+
+import ar.com.laboratorio.steady.pollo_corp.registro.aplication.ClientRegistryUseCase;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class PolloCorpApplicationTests {
-
+	@Autowired
+	ClientRegistryUseCase clientRegistryUseCase;
+	
 	@Test
 	void contextLoads() {
+		assertThat(clientRegistryUseCase).isNotNull();
 	}
 
 }
