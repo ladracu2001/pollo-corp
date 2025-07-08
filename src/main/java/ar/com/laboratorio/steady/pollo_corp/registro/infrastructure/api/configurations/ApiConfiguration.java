01820c59316bedb488da676387c8022f2022707c
ladracu2001 <ladracu2001@gmail.com>
2025-07-08 11:00:45 -0300

    Inyeccion de fileadapter.

--- src/main/java/ar/com/laboratorio/steady/pollo_corp/registro/infrastructure/api/configurations/ApiConfiguration.java
+++ src/main/java/ar/com/laboratorio/steady/pollo_corp/registro/infrastructure/api/configurations/ApiConfiguration.java
@@ -2,9 +2,10 @@ package ar.com.laboratorio.steady.pollo_corp.registro.infrastructure.api.configu
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.beans.factory.annotation.Value;
 import ar.com.laboratorio.steady.pollo_corp.registro.aplication.ClientRegistryUseCase;
 import ar.com.laboratorio.steady.pollo_corp.registro.dominio.ports.out.ClientRepository;
+import ar.com.laboratorio.steady.pollo_corp.registro.infrastructure.adapters.reports.FileAdapter;
 
 @Configuration
 public class ApiConfiguration {
@@ -12,5 +13,9 @@ public class ApiConfiguration {
     @Bean
     public ClientRegistryUseCase clientRegistryUseCase(ClientRepository clientRepository) {
         return new ClientRegistryUseCase(clientRepository);
+    }
+    @Bean
+    public FileAdapter fileAdapter(@Value("${file.base-directory:/tmp}") String baseDirectoryPath) {
+        return new FileAdapter(baseDirectoryPath);
     }
 }
